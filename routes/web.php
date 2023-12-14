78ffa66c52e1eab61118ab3c9af0dc1678a79454
gas <fireworld@i.ua>
2023-12-14 15:35:52 +0200

    add routes users

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,16 @@ Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function(){
         Route::get('/', 'IndexController');
     });
 
+    // Users
+    Route::group(['namespace' => 'User', 'prefix' => 'users'], function (){
+        Route::get('/', 'IndexController')->name('admin.user.index');
+        Route::get('/create', 'CreateController')->name('admin.user.create');
+        Route::post('/', 'StoreController')->name('admin.user.store');
+        Route::get('/{user}', 'ShowController')->name('admin.user.show');
+        Route::get('/{user}/edit', 'EditController')->name('admin.user.edit');
+        Route::patch('/{user}', 'UpdateController')->name('admin.user.update');
+        Route::delete('/{user}', 'DeleteController')->name('admin.user.delete');
+    });
 });
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
